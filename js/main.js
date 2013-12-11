918d24ca4b7cff0e55514cb1509db923b7009402
Emanuel Palm <emanuelpalm@gmail.com>
2013-12-11 19:00:13 +0100

    Minor changes to css and main.js.

--- js/main.js
+++ js/main.js
@@ -4,37 +4,116 @@ require.config({
 
 require(["webgl", "analyzer", "benchmark", "terminal"], function (WebGL, Analyzer, Benchmark, Terminal) {
 
-    var terminal = new Terminal.Terminal(document.getElementById("terminal"), 8);
+    var terminal = new Terminal.Terminal(document.getElementById("terminal"), 16);
     var startButton = document.getElementById("startButton");
     var frameTimeReports = [];
     var relativePerformanceReports = [];
 
-    terminal.addRow("Initializing WebGL ..............");
+    var STATIC_BENCHMARK_TIME = 20.0;
+    var staticBenchmarkParticleAmount = 0;
+
+    terminal.addRow("Initializing WebGL ..................");
 
 	var gl = initializeWebGL();
 
-    terminal.setTop("Initializing WebGL .............. done!");
-    terminal.addRow("Initializing benchmarker ........");
+    terminal.setTop("Initializing WebGL .................. done!");
+    terminal.addRow("Initializing benchmarker ............");
 
     var benchmarker = new Benchmark.Benchmarker(gl);
 
-    terminal.setTop("Initializing benchmarker ........ done!");
+    terminal.setTop("Initializing benchmarker ............ done!");
     terminal.addRow("Press START to start the Cloud Benchmark Suite.");
 
     startButton.style.visibility = "visible";
     startButton.onmousedown = function () {
         startButton.parentNode.removeChild(startButton);
 
-        terminal.addRow("Running cloud calibration .......");
-        benchmarker.runCalibrating(benchmarkEnd, 0.05, 1024);
+        benchmark0();
     };
 
-	function benchmarkEnd(amt, time) {
-        terminal.setTop("Running cloud calibration ....... done!");
+    function benchmark0() {
+        terminal.addRow("Running cloud calibration ...........");
+        benchmarker.setVBON(1);
+        benchmarker.setFBOTextureN(1);
+        benchmarker.runCalibrating(benchmark0done, 0.10, 1024);
+    }
 
-        relativePerformanceReports.push(amt);
+    function benchmark0done(particleAmount, time) {
+        terminal.setTop("Running cloud calibration ........... done!");
         terminal.addRow("Calibration took " + time.toFixed(2) + " seconds.");
-        terminal.addRow("Calibration converged at " + amt + " cloud particles.");
+        terminal.addRow("Calibration converged at " + particleAmount + " cloud particles.");
+        relativePerformanceReports.push(particleAmount);
+
+        staticBenchmarkParticleAmount = particleAmount;
+        benchmark1();
+    }
+
+    function benchmark1() {
+        terminal.addRow("Running frame time test {V=1 F=1} ... ");
+        benchmarker.setVBON(1);
+        benchmarker.setFBOTextureN(1);
+        benchmarker.runStatic(benchmark1done, STATIC_BENCHMARK_TIME, staticBenchmarkParticleAmount);
+    }
+
+    function benchmark1done(report) {
+        terminal.setTop("Running frame time test {V=1 F=1} ... done!");
+        terminal.addRow("Frame time average was " + report.average.toFixed(4) + ".");
+        terminal.addRow("Frame time standard deviation was " + report.standardDeviation.toFixed(4) + ".");
+        frameTimeReports.push(report);
+
+        benchmark2();
+    }
+
+    function benchmark2() {
+        terminal.addRow("Running frame time test {V=2 F=1} ... ");
+        benchmarker.setVBON(2);
+        benchmarker.setFBOTextureN(1);
+        benchmarker.runStatic(benchmark2done, STATIC_BENCHMARK_TIME, staticBenchmarkParticleAmount);
+    }
+
+    function benchmark2done(report) {
+        terminal.setTop("Running frame time test {V=2 F=1} ... done!");
+        terminal.addRow("Frame time average was " + report.average.toFixed(4) + ".");
+        terminal.addRow("Frame time standard deviation was " + report.standardDeviation.toFixed(4) + ".");
+        frameTimeReports.push(report);
+
+        benchmark3();
+    }
+
+    function benchmark3() {
+        terminal.addRow("Running frame time test {V=1 F=2} ... ");
+        benchmarker.setVBON(1);
+        benchmarker.setFBOTextureN(2);
+        benchmarker.runStatic(benchmark3done, STATIC_BENCHMARK_TIME, staticBenchmarkParticleAmount);
+    }
+
+    function benchmark3done(report) {
+        terminal.setTop("Running frame time test {V=1 F=2} ... done!");
+        terminal.addRow("Frame time average was " + report.average.toFixed(4) + ".");
+        terminal.addRow("Frame time standard deviation was " + report.standardDeviation.toFixed(4) + ".");
+        frameTimeReports.push(report);
+
+        benchmark4();
+    }
+
+    function benchmark4() {
+        terminal.addRow("Running frame time test {V=2 F=2} ... ");
+        benchmarker.setVBON(2);
+        benchmarker.setFBOTextureN(2);
+        benchmarker.runStatic(benchmark4done, STATIC_BENCHMARK_TIME, staticBenchmarkParticleAmount);
+    }
+
+    function benchmark4done(report) {
+        terminal.setTop("Running frame time test {V=2 F=2} ... done!");
+        terminal.addRow("Frame time average was " + report.average.toFixed(4) + ".");
+        terminal.addRow("Frame time standard deviation was " + report.standardDeviation.toFixed(4) + ".");
+        frameTimeReports.push(report);
+
+        benchmarkEnd();
+    }
+
+	function benchmarkEnd() {
+        terminal.addRow("All benchmarks successfully performed.");
 	}
 
 	function initializeWebGL() {
